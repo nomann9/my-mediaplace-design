40bf2da6dd1d83abb215ed32e1ecad4a6f18a4f0
nomann9 <noman9@gmail.com>
2026-08-30 00:47:59 +0530

    Fix preview asset mime types
    Serve local SVG and PNG assets with the correct content types so repo-managed icons and images render properly in the MediaPlace preview server.

--- preview-server.js
+++ preview-server.js
@@ -10,7 +10,9 @@ const mimeTypes = {
   ".html": "text/html; charset=utf-8",
   ".css": "text/css; charset=utf-8",
   ".js": "text/javascript; charset=utf-8",
-  ".json": "application/json; charset=utf-8"
+  ".json": "application/json; charset=utf-8",
+  ".png": "image/png",
+  ".svg": "image/svg+xml"
 };
 
 function send(res, status, body, type) {
